2274640748beb7eb3a1a152e9a97cbd712e748d3
Matthew-cell <87647086+Matthew-cell@users.noreply.github.com>
2021-07-21 20:46:31 +0800

    Add files via upload

---
+++ PYTHON WEDNESDAY HW.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 20:34:21 2021
+
+@author: USER
+"""
+
+import random
+x=random.randint(1,100)
+z=int(input('guess a number'))
+if x==z:
+    print:('right')
+else:
+    print('wrong')
+    print(x)
