7b4c6d8446b6c45046f3f48ab81560cb7f0629c7
Matthew-cell <87647086+Matthew-cell@users.noreply.github.com>
2021-07-22 21:04:16 +0800

    Add files via upload

---
+++ PYTHON.py thrusday HW.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 20:41:51 2021
+
+@author: USER
+"""
+
+for i in range(1, 10):
+    print('dragon ball' *i)
+    
+    
+for i in range(1, 10):
+    print('*' *i)
+    
+for i in range(1, 10):
+    print('iammatthew' *i)
