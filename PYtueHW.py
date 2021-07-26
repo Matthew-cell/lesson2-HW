ee3348efe70069297057be3b395f56c9c78fc6ac
Matthew-cell <87647086+Matthew-cell@users.noreply.github.com>
2021-07-26 14:01:17 +0800

    Add files via upload

---
+++ PYtueHW.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 23 11:56:32 2021
+
+@author: USER
+"""
+
+s=[]
+x = int(input("學生數量?"))
+g = 0
+for i in range(x):
+    y = int(input("學生成績?"))
+    s.append(y)
+    g = g+y
+    
+print(s)
+print(g/x)  
+for i in s:
+    print(i)
+   
